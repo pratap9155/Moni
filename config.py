0bc2644513c02669d8ca993e2bff6ab0bb4ec7a2
ARYANSONI9026 <soniaryan2240021@gmail.com>
2025-05-29 17:58:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ARYANSONI9026/ShraddhaMusicBot",
+    "https://github.com/ARYANSONI9026/DiscoMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
