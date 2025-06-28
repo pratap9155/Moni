9841fc7b7f4ac19729a3bad58e7dde15fd8a507a
pratap9155 <venompratap22@gmail.com>
2025-06-28 12:53:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Pubglovers_Shayri_love
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+2HpAd1kBDRo1NzY1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 
 # Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
 AUTO_GCAST = os.getenv("AUTO_GCAST")
